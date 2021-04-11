262ef54246ae6fd20153fa37f4caf330896f58af
Muhammad Putra Age <muhammadputraage@gmail.com>
2021-04-11 10:59:49 +0700

    Membuat halaman semua produk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,76 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Selamat Datang di DigiShop</h1>
+    <h1 class="heading-title">Semua Produk</h1>
+
+    <div class="card-container">
+      <a href="#" class="card">
+        <div class="card-img-container">
+          <img src="{{ asset('images/product-1.jpg') }}" alt="Product 1" class="card-img">
+        </div>
+
+        <div class="card-details">
+          <div>
+            <h3 class="card-title">Cool Headphone</h3>
+            <p class="card-stock">Stok: 30</p>
+          </div>
+          <p class="card-price">Rp. 200.000</p>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quasi expedita ad aspernatur quam neque minima placeat dicta incidunt?
+          </p>
+        </div>
+      </a>
+
+      <a href="#" class="card">
+        <div class="card-img-container">
+          <img src="{{ asset('images/product-1.jpg') }}" alt="Product 1" class="card-img">
+        </div>
+
+        <div class="card-details">
+          <div>
+            <h3 class="card-title">Cool Headphone</h3>
+            <p class="card-stock">Stok: 30</p>
+          </div>
+          <p class="card-price">Rp. 200.000</p>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quasi expedita ad aspernatur quam neque minima placeat dicta incidunt?
+          </p>
+        </div>
+      </a>
+
+      <a href="#" class="card">
+        <div class="card-img-container">
+          <img src="{{ asset('images/product-1.jpg') }}" alt="Product 1" class="card-img">
+        </div>
+
+        <div class="card-details">
+          <div>
+            <h3 class="card-title">Cool Headphone</h3>
+            <p class="card-stock">Stok: 30</p>
+          </div>
+          <p class="card-price">Rp. 200.000</p>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quasi expedita ad aspernatur quam neque minima placeat dicta incidunt?
+          </p>
+        </div>
+      </a>
+
+      <a href="#" class="card">
+        <div class="card-img-container">
+          <img src="{{ asset('images/product-1.jpg') }}" alt="Product 1" class="card-img">
+        </div>
+
+        <div class="card-details">
+          <div>
+            <h3 class="card-title">Cool Headphone</h3>
+            <p class="card-stock">Stok: 30</p>
+          </div>
+          <p class="card-price">Rp. 200.000</p>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt quasi expedita ad aspernatur quam neque minima placeat dicta incidunt?
+          </p>
+        </div>
+      </a>
+    </div>
+  </div>
 @endsection
